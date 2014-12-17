ad0384a95e4da5a34b4b2f69d568028205f26bd5
Ken L <klauer@bnl.gov>
2014-12-17 13:24:12 -0500

    Initial commit

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# TODO: use rcm? https://github.com/thoughtbot/rcm
+
+VUNDLE_PATH=~/.vim/bundle/Vundle.vim
+
+echo "* vim"
+ln -sf `pwd`/vim/ ~/.vim
+ln -sf ~/.vim/vimrc ~/.vimrc
+
+mkdir -p vim/bundle
+
+echo "** install vundle"
+if [ ! -d $VUNDLE_PATH ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git $VUNDLE_PATH
+fi
+
+echo "** installing bundles"
+vim -c ":BundleInstall"
+
+echo "* tmux"
+ln -sf `pwd`/tmux.conf ~/.tmux.conf
+
+echo "* bash/aliases"
+ln -sf `pwd`/aliases ~/.bash_aliases
