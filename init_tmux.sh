b660369558eac175e21f6559830b7838c0e8d6c3
K Lauer <klauer@bnl.gov>
2015-12-11 14:09:35 -0500

    tmux with plugins!

--- init_tmux.sh
+++ init_tmux.sh
@@ -1,18 +1,22 @@
 echo "* tmux"
+if [ -z "$DOTFILES" ]; then
+    DOTFILES=$PWD
+fi
+
 if [ ! -a $HOME/.tmux-all.conf ]; then
-    ln -sf `pwd`/tmux-all.conf $HOME/.tmux-all.conf
+    ln -sf $DOTFILES/tmux-all.conf $HOME/.tmux-all.conf
 else
     echo "** tmux configuration already exists"
 fi
 
 if [ ! -a $HOME/.tmux-1.9.conf ]; then
-    ln -sf `pwd`/tmux-1.9.conf $HOME/.tmux-1.9.conf
+    ln -sf $DOTFILES/tmux-1.9.conf $HOME/.tmux-1.9.conf
 else
     echo "** tmux-1.9 configuration already exists"
 fi
 
 if [ ! -a $HOME/.tmux-2.1.conf ]; then
-    ln -sf `pwd`/tmux-2.1.conf $HOME/.tmux-2.1.conf
+    ln -sf $DOTFILES/tmux-2.1.conf $HOME/.tmux-2.1.conf
 else
     echo "** tmux-2.1 configuration already exists"
 fi
@@ -22,8 +26,14 @@ TMUX_VERSION_MINOR="$(tmux -V | cut -d' ' -f2 | cut -d'.' -f2 | sed 's/[^0-9]*//
 
 if [ $TMUX_VERSION_MAJOR -eq 2 -a $TMUX_VERSION_MINOR -ge 1 ]; then
     echo "** Using tmux 2.1 config"
-    ln -sf `pwd`/tmux-2.1.conf $HOME/.tmux.conf
+    ln -sf $DOTFILES/tmux-2.1.conf $HOME/.tmux.conf
 else
     echo "** Using tmux 1.9 config"
-    ln -sf `pwd`/tmux-1.9.conf $HOME/.tmux.conf
+    ln -sf $DOTFILES/tmux-1.9.conf $HOME/.tmux.conf
+fi
+
+if [ ! -d $HOME/.config/tmux ]; then
+    ln -sf $DOTFILES/tmux $HOME/.config/tmux
+else
+    echo "** tmux xdg config path exists"
 fi
