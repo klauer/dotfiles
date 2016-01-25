c548cec3d3c2ad4133c46bd3244287a206eb7aae
K Lauer <klauer@bnl.gov>
2016-01-25 09:47:39 -0500

    Use $DOTFILES

--- init.sh
+++ init.sh
@@ -3,17 +3,19 @@
 # TODO: use rcm? https://github.com/thoughtbot/rcm
 # TODO: or dotbot? https://github.com/anishathalye/dotbot
 
+DOTFILES=`pwd`
+
 # -- bash
 echo "* bash"
 echo "** aliases"
-ln -sf `pwd`/aliases $HOME/.bash_aliases
+ln -sf $DOTFILES/aliases $HOME/.bash_aliases
 
-conf_line="$(grep `pwd`/bashrc $HOME/.bashrc)"
+conf_line="$(grep $DOTFILES/bashrc $HOME/.bashrc)"
 
 echo "** source bashrc"
 if [ -z "$conf_line" ]; then
     echo "Adding bashrc config"
-    echo "source `pwd`/bashrc" >> $HOME/.bashrc
+    echo "source $DOTFILES/bashrc" >> $HOME/.bashrc
     source $HOME/.bashrc
 else
     echo "bashrc config already set"
@@ -24,7 +26,7 @@ echo "* gdb"
 
 if [ ! -d $HOME/.gdbinit ]; then
     wget -O gdbinit https://raw.githubusercontent.com/cyrus-and/gdb-dashboard/master/.gdbinit
-    ln -sf `pwd`/gdbinit $HOME/.gdbinit
+    ln -sf $DOTFILES/gdbinit $HOME/.gdbinit
 else
     echo "** gdb init already exists"
 fi
@@ -42,7 +44,7 @@ source init_tmux.sh
 # -- git
 echo "* git"
 if [ ! -a $HOME/.gitconfig ]; then
-    ln -sf `pwd`/gitconfig $HOME/.gitconfig
+    ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
 else
     echo "** git configuration already exists"
 fi
@@ -50,7 +52,7 @@ fi
 # -- hg
 echo "* hg"
 if [ ! -a $HOME/.hgrc ]; then
-    ln -sf `pwd`/hgrc $HOME/.hgrc
+    ln -sf $DOTFILES/hgrc $HOME/.hgrc
 else
     echo "** hg configuration already exists"
 fi
@@ -58,7 +60,7 @@ fi
 # -- conda
 echo "* conda"
 if [ ! -a $HOME/.condarc ]; then
-    ln -sf `pwd`/condarc $HOME/.condarc
+    ln -sf $DOTFILES/condarc $HOME/.condarc
 else
     echo "** conda configuration already exists"
 fi
@@ -69,7 +71,7 @@ echo "* binstar"
 mkdir -p $HOME/.config/binstar
 
 if [ ! -a $HOME/.config/binstar/config.yaml ]; then
-    ln -sf `pwd`/binstar_config.yaml $HOME/.config/binstar/config.yaml
+    ln -sf $DOTFILES/binstar_config.yaml $HOME/.config/binstar/config.yaml
 else
     echo "** binstar configuration already exists"
 fi
@@ -77,7 +79,7 @@ fi
 # -- flake8 settings
 echo "* flake8"
 if [ ! -a $HOME/.config/flake8 ]; then
-    ln -sf `pwd`/flake8 $HOME/.config/flake8
+    ln -sf $DOTFILES/flake8 $HOME/.config/flake8
 else
     echo "** flake8 configuration already exists"
 fi
