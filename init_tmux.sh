40d1f848392a714c6c2f6acd2318149db445df1e
K Lauer <klauer@slac.stanford.edu>
2020-05-02 12:41:27 -0700

    remove tmux 1.9 conf

--- init_tmux.sh
+++ init_tmux.sh
@@ -3,33 +3,10 @@ if [ -z "$DOTFILES" ]; then
     DOTFILES=$PWD
 fi
 
-if [ ! -a $HOME/.tmux-all.conf ]; then
-    ln -sf $DOTFILES/tmux-all.conf $HOME/.tmux-all.conf
+if [ ! -a $HOME/.tmux.conf ]; then
+    ln -sf $DOTFILES/tmux.conf $HOME/.tmux.conf
 else
     echo "** tmux configuration already exists"
-fi
-
-if [ ! -a $HOME/.tmux-1.9.conf ]; then
-    ln -sf $DOTFILES/tmux-1.9.conf $HOME/.tmux-1.9.conf
-else
-    echo "** tmux-1.9 configuration already exists"
-fi
-
-if [ ! -a $HOME/.tmux-2.1.conf ]; then
-    ln -sf $DOTFILES/tmux-2.1.conf $HOME/.tmux-2.1.conf
-else
-    echo "** tmux-2.1 configuration already exists"
-fi
-
-TMUX_VERSION_MAJOR="$(tmux -V | cut -d' ' -f2 | cut -d'.' -f1 | sed 's/[^0-9]*//g')"
-TMUX_VERSION_MINOR="$(tmux -V | cut -d' ' -f2 | cut -d'.' -f2 | sed 's/[^0-9]*//g')"
-
-if [ $TMUX_VERSION_MAJOR -eq 2 -a $TMUX_VERSION_MINOR -ge 1 ]; then
-    echo "** Using tmux 2.1 config"
-    ln -sf $DOTFILES/tmux-2.1.conf $HOME/.tmux.conf
-else
-    echo "** Using tmux 1.9 config"
-    ln -sf $DOTFILES/tmux-1.9.conf $HOME/.tmux.conf
 fi
 
 if [ ! -d $HOME/.config/tmux ]; then
