2d89b27fd174ffb2fc7d6f2eefd89426f598c44b
K Lauer <kenneth.lauer@gmail.com>
2022-03-25 13:29:01 -0700

    better gutentags setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -847,6 +847,62 @@ dap.adapters.python = {
   args = { '-m', 'debugpy.adapter' };
 }
 
+-- Tags
+
+vim.g.gutentags_ctags_exclude = {
+    '*.git', '*.svg', '*.hg',
+    '*/tests/*',
+    'build',
+    'dist',
+    '*sites/*/files/*',
+    'bin',
+    'node_modules',
+    'bower_components',
+    'cache',
+    'compiled',
+    'docs',
+    'example',
+    'bundle',
+    'vendor',
+    '*.md',
+    '*-lock.json',
+    '*.lock',
+    '*bundle*.js',
+    '*build*.js',
+    '.*rc*',
+    '*.json',
+    '*.min.*',
+    '*.map',
+    '*.bak',
+    '*.zip',
+    '*.pyc',
+    '*.class',
+    '*.sln',
+    '*.Master',
+    '*.csproj',
+    '*.tmp',
+    '*.csproj.user',
+    '*.cache',
+    '*.pdb',
+    'tags*',
+    'cscope.*',
+    '*.exe', '*.dll',
+    '*.swp', '*.swo',
+    '*.bmp', '*.gif', '*.ico', '*.jpg', '*.png',
+    '*.rar', '*.zip', '*.tar', '*.tar.gz', '*.tar.xz', '*.tar.bz2',
+    '*.pdf', '*.doc', '*.docx', '*.ppt', '*.pptx',
+    }
+
+vim.g.gutentags_add_default_project_roots = false
+vim.g.gutentags_project_root = {'package.json', '.git'}
+vim.g.gutentags_cache_dir = vim.fn.expand('~/.cache/ctags/')
+vim.g.gutentags_generate_on_new = true
+vim.g.gutentags_generate_on_missing = true
+vim.g.gutentags_generate_on_write = true
+vim.g.gutentags_generate_on_empty_buffer = true
+vim.cmd([[command! -nargs=0 GutentagsClearCache call system('rm ' . g:gutentags_cache_dir . '/*')]])
+vim.g.gutentags_ctags_extra_args = {'--tag-relative=yes', '--fields=+ailmnS', }
+
 -- Text object customization / column selection
 -- vim.g.textobj_wordcolumn_no_default_key_mappings = 1
 
